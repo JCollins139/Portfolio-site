b94ffc4d818bf10d6c8e11061f9632d5ad09a08e
Joe Collins <joecollins@Joes-MacBook-Pro.local>
2019-03-08 23:35:23 -0500

    lang=en

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My portfolio website"
+    @page_title = "Joe Collins"
     @seo_keywords = "Joe Collins portfolio"
   end
 end
