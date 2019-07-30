eaa83054af54634ab9158001c892575d77010891
Joe Collins <joecollins@Joes-MacBook-Pro.local>
2019-07-30 17:00:03 -0400

    try again

--- README.md
+++ README.md
@@ -10,15 +10,22 @@ Things you may want to cover:
 * System dependencies
 
 * Configuration
+- Run bundle install
+- Run yarn install
 
 * Database creation
+  configured for a Postgres db
+  - Run rails db:create
 
 * Database initialization
+  - Run rails db:migrate
 
 * How to run the test suite
 
 * Services (job queues, cache servers, search engines, etc.)
-
+  to start the server run
+  - Run rails s
+  - visit localhost:3000 in address bar to launch site
 * Deployment instructions
 
 * ...
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,10 +1,11 @@
 class Portfolio < ApplicationRecord
   has_many :technologies, dependent: :destroy
   accepts_nested_attributes_for :technologies,
+                                allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank?}
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  # include Placeholder
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
@@ -18,11 +19,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height: '650', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
-  end
 end
